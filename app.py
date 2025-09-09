994032bd9101898958ce188735f0cd4887e36588
Bharat Singh <130927028+BharatSingh09@users.noreply.github.com>
2025-09-09 16:22:49 +0530

    Update app.py
    added rfid tag seq

--- app.py
+++ app.py
@@ -6,9 +6,11 @@ import io
 app = Flask(__name__)
 pattern_1 = re.compile(r'appConfig.s4lgcConfig.routes\[(\d+)U\]\.distEntryExitSignal')
 pattern_2=re.compile(r'appConfig.s4lgcConfig.routes\[(\d+)U\]\.ssp\.speedInfo\[(\d+)U\]\.dist')
+pattern_3=re.compile(r'appConfig.s4lgcConfig.routes\[(\d+)U\]\.entrySigTagId')
 my_dict={
     1:(16,pattern_1),
-    2:(34,pattern_2)
+    2:(34,pattern_2),
+    3:(15,pattern_3)
 }
 
 # def load_csv1(file):
@@ -74,6 +76,95 @@ def load_csv2_1(file,pattern):
     return mapping
 
 
+def load_csv2_2(file, pattern):
+    mapping = {}
+    reader = list(csv.reader(io.StringIO(file.read().decode('utf-8'))))
+
+    i = 0
+    while i < len(reader):
+        row = reader[i]
+        if len(row) < 4:
+            i += 1
+            continue
+
+        match = pattern.search(row[0])
+        if match:
+            sno = int(match.group(1)) + 1
+            sub_pattern_1 = re.compile(rf'appConfig.s4lgcConfig.routes\[{sno-1}U\]\.enRouteTags\[(\d+)U\]\.linkDistance')
+            sub_pattern_2=re.compile(rf'appConfig.s4lgcConfig.routes\[{sno-1}U\]\.enRouteTags\[(\d+)U\]\.tagId')
+            # scan next rows until mismatch
+            j = i + 2
+            while j < len(reader):
+                row2 = reader[j]
+                sub_match_1 = sub_pattern_1.search(row2[0])
+                sub_match_2 = sub_pattern_2.search(reader[j+1][0])
+                if not  sub_match_1 or not sub_match_2:
+                    break   # stop at first non-matching row
+
+                #tag_idx = int(sub_match_1.group(1))
+                value=row2[3]
+                id=reader[j+1][3]
+                if id=='0':
+                    break
+                if sno in mapping:
+                    mapping[sno][0].append(j+1)
+                    mapping[sno][1].append(value)
+                    mapping[sno][2].append(id)
+                else:
+                    mapping[sno] = ([j+1], [value],[id])
+                j += 2
+
+            # skip ahead
+            i = j
+        else:
+            i += 1
+    return mapping
+
+
+def compare_data_2(csv1_data, csv2_data, column_index):
+    results = []
+
+    for row in csv1_data:
+        try:
+            sno = int(row[0])  # Get S.No from the first column
+            csv1_val = row[column_index].strip()
+            csv1_val = csv1_val.split(")#(")
+
+
+            line_no, dist, id = csv2_data.get(sno, ([], [], []))  # Use empty lists if not found
+
+            for i in range(len(csv1_val)):
+                csv1_value = csv1_val[i].replace('(', '').replace(')', '')
+                csv1_parts = csv1_value.split('#')
+
+                csv1_item_1 = csv1_parts[0] if len(csv1_parts) > 0 else 'Invalid'
+                csv1_item_2 = csv1_parts[1] if len(csv1_parts) > 0 else 'Invalid'
+                # Default values if index out of range
+                index_val = line_no[i] if i < len(line_no) else 'Missing'
+                dist_val = dist[i] if i < len(dist) else 'Missing'
+                id_val=id[i] if i < len(id) else 'Missing'
+
+                # Determine match status
+                if i < len(dist) and dist[i] is not None:
+                    match_status = 'Match' if csv1_item_1 == dist[i] else 'Mismatch'
+                else:
+                    match_status = 'Missing'
+
+                results.append({
+                    'S.No': sno,
+                    'CSV1_Dist': csv1_item_1,
+                    'CSV1_RFID':csv1_item_2,
+                    'Index': index_val,
+                    'CSV2_Dist': dist_val,
+                    'CSV2_RFID':id_val,
+                    'Status': match_status
+                })
+        except Exception as e:
+            print(f"Exception: {e}")
+            pass
+    return results
+
+
 def compare_data(csv1_data, csv2_data, column_index):
     results = []
     for row in csv1_data:
@@ -135,12 +226,18 @@ def index():
         csv2_file = request.files.get('csv2_file')
         selected_value = request.form.get('selected')
         value = my_dict[int(selected_value)]
-        column_name,pattern=value
+        column_index,pattern=value
         if csv1_file and csv2_file:
             try:
                 csv1_data = load_csv1(csv1_file)
-                csv2_data = load_csv2_1(csv2_file,pattern)
-                result_data = compare_data(csv1_data, csv2_data,column_name)
+                if (selected_value=="3"):
+
+                    csv2_data = load_csv2_2(csv2_file,pattern)
+                    result_data = compare_data_2(csv1_data, csv2_data,column_index)
+                else:
+
+                    csv2_data = load_csv2_1(csv2_file,pattern)
+                    result_data = compare_data(csv1_data, csv2_data,column_index)
             except Exception as e:
                 return render_template('index.html', error=str(e), result_data=[])
     return render_template('index.html', result_data=result_data)
